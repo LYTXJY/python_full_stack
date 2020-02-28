0adfb8bce3bc52b3ec0b392d44b2909067da1431
xjy <18822148155@163.com>
2020-02-28 14:39:46 +0800

    add 2&3

---
+++ Code/src/hellopython/1code_stytle.py
@@ -0,0 +1,15 @@
+print(1+2*3/4+(1+(1+(3.2+2/3*4))))
+print(  1+2*3/4+(   1+( 1+  (3.2+2/3*4)   )   ) )
+
+print(  1+2*3/4+(   1+( 1+  (3.2+2/3*4)   )   ) )
+
+num = 4.6
+num1 = int(num)
+num2 = round(num)
+print(num2)
+print(type(num2))
+
+num = "4.5"
+num = eval(num)
+print(num)
+print(type(num))
