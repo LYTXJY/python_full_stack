b7393333369d36f3e12f20ce49937cae497e921b
xjy <18822148155@163.com>
2020-03-02 23:42:47 +0800

    turtle

---
+++ Code/src/hellopython/os/myos.py
@@ -0,0 +1,14 @@
+import os
+
+os.system("calc")
+#os.system("taskkill /f /im calc.exe")
+
+#
+# os.system("mspaint")
+# os.system("task kill /f /im mspaint.exe")
+
+#os.system("ipconfig")
+
+os.system("clac")
+os.system("mspaint")
+os.system("taskkill /f /im mspaint.exe")
